24f299fd12f908dbdae86e1e97696ceb87260c74
pazgu <pazgueta@gmail.com>
2026-07-10 15:55:14 +0300

    build: upgrade to tailwind v4 and fix shadcn forms

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -1,22 +1,24 @@
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
+} from "@/components/ui/card"
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
-} from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
 
-export function LoginForm({ className, ...props }) {
+export function LoginForm({
+  className,
+  ...props
+}) {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -31,35 +33,26 @@ export function LoginForm({ className, ...props }) {
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
+                <Input id="email" type="email" placeholder="m@example.com" required />
               </Field>
               <Field>
                 <div className="flex items-center">
                   <FieldLabel htmlFor="password">Password</FieldLabel>
                   <a
                     href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    {/* Forgot your password? */}
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                    Forgot your password?
                   </a>
                 </div>
                 <Input id="password" type="password" required />
               </Field>
               <Field>
                 <Button type="submit">Login</Button>
-                {/* <Button variant="outline" type="button">
+                <Button variant="outline" type="button">
                   Login with Google
-                </Button> */}
+                </Button>
                 <FieldDescription className="text-center">
-                  Don&apos;t have an account?
-                  <Link to="/signup" className="text-primary underline">
-                    Sign up
-                  </Link>
+                  Don&apos;t have an account? <a href="#">Sign up</a>
                 </FieldDescription>
               </Field>
             </FieldGroup>
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import { LoginForm } from "../../components/ui/login-form";
+import { LoginForm } from "@/components/login-form";
 
 export default function Login() {
   return (
@@ -7,5 +7,6 @@ export default function Login() {
         <LoginForm />
       </div>
     </div>
+  );
   );
 }
