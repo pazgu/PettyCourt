456b9a99c00ce82369f82d0c452e80e5c4fae706
doronm <doronm@chain-reaction.io>
2026-07-10 09:19:00 +0300

    Add signup form and conneted the routing links

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,12 +1,17 @@
 import { Button } from "../components/ui/button";
+import { Link } from "react-router-dom";
 
 export default function Navbar(isLoggedIn) {
   return (
     <header className="flex items-center justify-between px-4 py-2 border-b bg-background">
       <div className="font-semibold">Objection</div>
       <nav className="flex gap-2">
-        <Button variant="ghost">Signup</Button>
-        <Button variant="default">Login</Button>
+        <Link to="/signup">
+          <Button variant="ghost">Signup</Button>
+        </Link>
+        <Link to="/">
+          <Button variant="default">Login</Button>
+        </Link>
       </nav>
     </header>
   );
