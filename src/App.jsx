5460610e799246128cea5c6ea8394a064c3575d9
doronm <doronm@chain-reaction.io>
2026-07-12 12:31:59 +0300

    Load user data on app initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
 import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home/Home";
 import AddCaseModal from "./components/shared/cases/AddCaseModal";
+import { authStore } from "@/store/AuthStore";
 
 export default function App() {
+  useEffect(() => {
+    authStore.loadUser();
+  }, []);
+
   return (
     <BrowserRouter>
       <Navbar />
