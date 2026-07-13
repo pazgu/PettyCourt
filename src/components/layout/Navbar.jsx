8f75ebe43de204ea14503f72e2ec88ac12ed5dae
doronm <doronm@chain-reaction.io>
2026-07-13 07:40:41 +0300

    Add navigation to logout button and filter cases by plaintiff ID

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,10 +1,11 @@
 import { observer } from "mobx-react-lite";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { User } from "lucide-react";
 import { authStore } from "@/store/AuthStore";
 
 export default observer(function Navbar({ isLoggedIn }) {
+  const navigate = useNavigate();
   return (
     <header className="flex items-center justify-between px-4 py-2 border-b bg-background">
       <Link to="/">
@@ -23,7 +24,9 @@ export default observer(function Navbar({ isLoggedIn }) {
               </Button>
             </Link>
 
-            <Button variant="default" onClick={() => authStore.logout()}>
+            <Button variant="default" onClick={
+              () => {authStore.logout()
+              navigate("/login")}}>
               Logout
             </Button>
           </>
