0c4b427f2d2c040b08ea9dc90fda2987f78ad201
Andrey Ruch <andreyruch1@gmail.com>
2026-07-13 17:09:27 +0300

    Update Navbar to replace User icon with BriefcaseBusiness and adjust layout for better spacing

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,13 +1,13 @@
 import { observer } from "mobx-react-lite";
 import { Button } from "../ui/button";
 import { Link, useNavigate } from "react-router-dom";
-import { User, Gavel } from "lucide-react";
+import { Gavel, BriefcaseBusiness } from "lucide-react";
 import { authStore } from "@/store/AuthStore";
 
 export default observer(function Navbar() {
   const navigate = useNavigate();
   const isLoggedIn = !!authStore.user;
-  
+
   return (
     <header className="flex items-center justify-between px-4 py-2 border-b bg-background">
       <Link to="/">
@@ -19,21 +19,22 @@ export default observer(function Navbar() {
         </div>
       </Link>
 
-      <nav className="flex items-center gap-2">
+      <nav className="flex items-center gap-4">
         {isLoggedIn ? (
           <>
-            <Link to="/my-cases">
-              <div>My Cases</div>
-            </Link>
-            <Link to="#">
-              <Button variant="ghost">
-                <User className="h-4 w-4" />
-              </Button>
+            <Link to="/my-cases" className="flex items-center gap-1.5">
+              <BriefcaseBusiness className="h-4 w-4" />
+              My Cases
             </Link>
 
-            <Button variant="default" onClick={
-              () => {authStore.logout()
-              navigate("/login")}}>
+            <Button
+              variant="outline"
+              className="cursor-pointer"
+              onClick={() => {
+                authStore.logout();
+                navigate("/login");
+              }}
+            >
               Logout
             </Button>
           </>
